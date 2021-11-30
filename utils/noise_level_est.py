9d5e5bf3750cf08b1ed5e738d3af3eb17cc5efa8
lixiaopeng123456 <54618126+lixiaopeng123456@users.noreply.github.com>
2021-11-30 23:40:23 +0800

    Add files via upload

---
+++ utils/noise_level_est.py
@@ -0,0 +1,60 @@
+import sys
+import numpy as np
+
+
+def im2patch(im, pch_size, stride=1):
+    '''
+    Transform image to patches.
+    Input:
+        im: 3 x H x W or 1 X H x W image, numpy format
+        pch_size: (int, int) tuple or integer
+        stride: (int, int) tuple or integer
+    '''
+    if isinstance(pch_size, tuple):
+        pch_H, pch_W = pch_size
+    elif isinstance(pch_size, int):
+        pch_H = pch_W = pch_size
+    else:
+        sys.exit('The input of pch_size must be a integer or a int tuple!')
+
+    if isinstance(stride, tuple):
+        stride_H, stride_W = stride
+    elif isinstance(stride, int):
+        stride_H = stride_W = stride
+    else:
+        sys.exit('The input of stride must be a integer or a int tuple!')
+
+    C, H, W = im.shape
+    num_H = len(range(0, H - pch_H + 1, stride_H))
+    num_W = len(range(0, W - pch_W + 1, stride_W))
+    num_pch = num_H * num_W
+    pch = np.zeros((C, pch_H * pch_W, num_pch), dtype=im.dtype)
+    kk = 0
+    for ii in range(pch_H):
+        for jj in range(pch_W):
+            temp = im[:, ii:H - pch_H + ii + 1:stride_H, jj:W - pch_W + jj + 1:stride_W]
+            pch[:, kk, :] = temp.reshape((C, num_pch))
+            kk += 1
+
+    return pch.reshape((C, pch_H, pch_W, num_pch))
+
+
+def get_noise_sigma(noise_img, pch_size=8):
+    im = noise_img
+
+    # image to patch
+    pch = im2patch(im, pch_size, 3)  # C x pch_size x pch_size x num_pch tensor
+    num_pch = pch.shape[3]
+    pch = pch.reshape((-1, num_pch))  # d x num_pch matrix
+    d = pch.shape[0]
+
+    mu = pch.mean(axis=1, keepdims=True)  # d x 1
+    X = pch - mu
+    sigma_X = np.matmul(X, X.transpose()) / num_pch
+    sig_value, _ = np.linalg.eigh(sigma_X)
+    sig_value.sort()
+
+    for ii in range(-1, -d - 1, -1):
+        tau = np.mean(sig_value[:ii])
+        if np.sum(sig_value[:ii] > tau) == np.sum(sig_value[:ii] < tau):
+            return np.sqrt(tau) *255
